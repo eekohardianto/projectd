877a2237b8a6849660d53b97e14faf23383a3748
eekohardianto <eekohardianto@gmail.com>
2021-10-31 21:53:25 +0700

    pasang spatie permission

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Gate For Learner',
+            'username' => 'admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('admin123')
+        ]);
+
+        $role = Role::create(['name' => 'Admin']);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
